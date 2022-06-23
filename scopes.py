c184aeb68ad85616d219c5dce952abbf02184149
Devin Moreland <devinmoreland20@gmail.com>
2022-06-22 20:18:56 -0500

    ACG

---
+++ scopes.py
@@ -0,0 +1,13 @@
+y = 5
+
+def set_x(z):
+    x = z
+    global y 
+    global a
+    y = x
+    a = 7
+print("y before set_x:", y)
+
+set_x(10)
+print("y after set_x:", y)
+print("a after set_x:", a)
