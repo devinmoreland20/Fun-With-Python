5982125ef8ce259a37445377c4e623606384ce64
devinmoreland <devinmoreland20@gmail.com>
2022-06-28 23:11:20 -0500

    Python project2

--- project.py
+++ project.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3.9
+def instances():
+    ec2 = int(input("How many EC2 instance do you want: ")) 
+    dept = str(input("What department do you work in? "))
+    dept = dept.lower()
+    for name in ["marketing", "accounting", "finops"]:
+        s = name
+    if dept == s:  
+        print("Below is your list:")   
+    else:
+        print("You should not be using this genrator")
+        exit()
 
-ec2 = int(input("How many EC2 instance do you want: "))
-dept = str(input("What department do you work in? "))
-#print(ec2)
-def gen(ec2):
-    for i in range(ec2):
-        yield i
-for i in gen(ec2):
-    i = id(i)
-    n = (dept + str(i))
-    print(n)
+    def gen(ec2):
+        for i in range(ec2):
+            yield i
+    
+    for i in gen(ec2):
+        i = id(i)
+        n = (dept + str(i))
+        print(n)
+
+instances()
+
+
 
 
 
