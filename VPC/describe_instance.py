1cd5f078ad7fed2b6825abd03bdd41e98afe60a4
devinmoreland <devinmoreland20@gmail.com>
2022-07-05 08:44:19 -0500

    newer

---
+++ VPC/describe_instance.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.9
+
+import boto3
+
+ec2 = boto3.client('ec2')
+
+x = ec2.describe_instances()
+
+data=x["Reservations"][0]
+data_instance=data["Instances"]
+
+for i in range (len(data_instance)):
+    print(f"instance id is {data_instance[i]['InstanceId']}")
+
